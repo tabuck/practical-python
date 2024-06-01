3cae4062aa929330c8efb7a5c1e03582a6730e0d
Thomas <tabuck@gmail.com>
2024-06-01 07:26:10 -0400

    Completed Exercise 1.6

--- Work/sears.py
+++ Work/sears.py
@@ -1,7 +1,9 @@
-bill_thickness = 0.11 * 0.001 # Meters (0.11 mm)
-sears_height = 442 # Height (meters)
-num_bills = 1
-day = 1
+# sears.py
+
+bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
+sears_height   = 442             # Height (meters)
+num_bills      = 1
+day            = 1
 
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
@@ -11,4 +13,3 @@ while num_bills * bill_thickness < sears_height:
 print('Number of days', day)
 print('Number of bills', num_bills)
 print('Final height', num_bills * bill_thickness)
-
